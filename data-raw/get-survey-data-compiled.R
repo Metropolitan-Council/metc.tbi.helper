b2d8df3f97d687cc269a808a2d8de114d65aace5
Ashley Asmus <ashley.asmus@gmail.com>
2022-06-06 12:55:33 -0500

    write compiled tables to csv

---
+++ data-raw/get-survey-data-compiled.R
@@ -0,0 +1,22 @@
+tbidb <- ROracle::dbConnect(
+  dbDriver("Oracle"),
+  dbname = keyring::key_get("mts_planning_database_string"),
+  username = "mts_planning_data",
+  password = keyring::key_get("mts_planning_data_pw")
+)
+
+ROracle::dbReadTable(tbidb, "tbi_19_day_public") %>%
+  write.csv("data/data-csv/day.csv", row.names = F)
+
+ROracle::dbWriteTable(tbidb, "tbi_19_trip_public")%>%
+  write.csv("data/data-csv/trip.csv", row.names = F)
+
+ROracle::dbWriteTable(tbidb, "tbi_19_hh_public")%>%
+  write.csv("data/data-csv/hh.csv", row.names = F)
+
+ROracle::dbWriteTable(tbidb, "tbi_19_veh_public")%>%
+  write.csv("data/data-csv/veh.csv", row.names = F)
+
+ROracle::dbWriteTable(tbidb, "tbi_19_per_public")%>%
+  write.csv("data/data-csv/per.csv", row.names = F)
+
