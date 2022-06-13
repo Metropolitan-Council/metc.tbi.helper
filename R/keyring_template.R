af0fe929198eec4375b7a9c59eaf6cd4e6217f99
Ashley Asmus <ashley.asmus@gmail.com>
2022-06-13 10:30:46 -0500

    create keyring template

---
+++ R/keyring_template.R
@@ -0,0 +1,9 @@
+# Keyring Set up
+# If you need the keys to the database, get in touch with Ashley Asmus or Sean Molloy
+
+keyring::key_set("mts_planning_database_string")
+# looks like: (DESCRIPTION=(ADDRESS_LIST ....
+# paste this in, NO QUOTE MARKS around the string.
+
+keyring::key_set("mts_planning_data_pw")
+# type this in, NO QUOTES!
