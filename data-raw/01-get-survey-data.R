ca4eee65d5317effa87673fcfb839724666f6921
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-06-17 12:24:26 -0500

    copy pasta for 2021 data

--- data-raw/01-get-survey-data.R
+++ data-raw/01-get-survey-data.R
@@ -11,7 +11,7 @@ Sys.setenv(ORA_SDTZ = "America/Chicago")
 
 ## connect to database ------------
 tbidb <- ROracle::dbConnect(
-  dbDriver("Oracle"),
+  DBI::dbDriver("Oracle"),
   dbname = keyring::key_get("mts_planning_database_string"),
   username = "mts_planning_data",
   password = keyring::key_get("mts_planning_data_pw")
@@ -19,19 +19,34 @@ tbidb <- ROracle::dbConnect(
 
 ## Load tables ---------
 message("Loading raw data from Oracle database")
-hh <- ROracle::dbReadTable(tbidb, "TBI19_HOUSEHOLD_RAW") %>% as.data.table()
-per <- ROracle::dbReadTable(tbidb, "TBI19_PERSON_RAW") %>% as.data.table()
-trip <- ROracle::dbReadTable(tbidb, "TBI19_TRIP_RAW") %>% as.data.table()
-veh <- ROracle::dbReadTable(tbidb, "TBI19_VEHICLE_RAW") %>% as.data.table()
-day <- ROracle::dbReadTable(tbidb, "TBI19_DAY_RAW") %>% as.data.table()
+hh19 <- ROracle::dbReadTable(tbidb, "TBI19_HOUSEHOLD_RAW") %>% as.data.table()
+per19 <- ROracle::dbReadTable(tbidb, "TBI19_PERSON_RAW") %>% as.data.table()
+trip19 <- ROracle::dbReadTable(tbidb, "TBI19_TRIP_RAW") %>% as.data.table()
+veh19 <- ROracle::dbReadTable(tbidb, "TBI19_VEHICLE_RAW") %>% as.data.table()
+day19 <- ROracle::dbReadTable(tbidb, "TBI19_DAY_RAW") %>% as.data.table()
+
+hh21 <- ROracle::dbReadTable(tbidb, "TBI21_HOUSEHOLD_RAW") %>% as.data.table()
+per21 <- ROracle::dbReadTable(tbidb, "TBI21_PERSON_RAW") %>% as.data.table()
+trip21 <- ROracle::dbReadTable(tbidb, "TBI21_TRIP_RAW") %>% as.data.table()
+veh21 <- ROracle::dbReadTable(tbidb, "TBI21_VEHICLE_RAW") %>% as.data.table()
+day21 <- ROracle::dbReadTable(tbidb, "TBI21_DAY_RAW") %>% as.data.table()
 
 ## Translate tables using dictionary -----------
 message("Translating tables using dictionary")
-dictionary <-
+
+dictionary19 <-
   ROracle::dbReadTable(tbidb, "TBI19_DICTIONARY") %>%
   select(-table) %>%
   unique() %>%
   as.data.table()
+
+dictionary21 <-
+  ROracle::dbReadTable(tbidb, "TBI21_DICTIONARY") %>%
+  select(-table) %>%
+  unique() %>%
+  as.data.table()
+
+dictionary21 <- rename(dictionary21, "value_label" = label)
 
 # note: this part uses data.table syntax and functions.
 translate_tbi <- function(dat, dictionary) {
@@ -78,7 +93,7 @@ translate_tbi <- function(dat, dictionary) {
   namevec <- names(newdat)
   for (i in namevec) {
     if (i %in% unique(dictionary$variable)) {
-      col_levels <- dictionary$value_label[dictionary$variable == i]
+      col_levels <- unique(dictionary$value_label[dictionary$variable == i])
       newdat[, (i) := factor(get(i), levels = col_levels)]
     }
   }
@@ -87,26 +102,43 @@ translate_tbi <- function(dat, dictionary) {
 }
 
 message("...person...")
-per <- translate_tbi(per, dictionary)
+per19 <- translate_tbi(per19, dictionary19)
+per21 <- translate_tbi(per21, dictionary21)
+
 message("...hh...")
-hh <- translate_tbi(hh, dictionary)
+hh19 <- translate_tbi(hh19, dictionary19)
+hh21 <- translate_tbi(hh21, dictionary21)
+
 message("...veh...")
-veh <- translate_tbi(veh, dictionary)
+veh19 <- translate_tbi(veh19, dictionary19)
+veh21 <- translate_tbi(veh21, dictionary21)
+
 message("...day...")
-day <- translate_tbi(day, dictionary)
+day19 <- translate_tbi(day19, dictionary19)
+day21 <- translate_tbi(day21, dictionary21)
+
 message("...trip...")
-trip <- translate_tbi(trip, dictionary)
+trip19 <- translate_tbi(trip19, dictionary19)
+trip21 <- translate_tbi(trip21, dictionary21)
 
 # Replace missing with NA -----------
 message("Replacing missing values with NA")
 # all the numeric codes for missing:
 all_missing_codes <-
-  dictionary[grep("Missing", value_label), "value", with = F]
+  rbind(
+    dictionary19[grep("Missing", value_label), "value", with = F],
+    dictionary21[grep("Missing", value_label), "value", with = F],
+    use.names = T)
+
 all_missing_codes <- unique(all_missing_codes$value)
 
 # all the value labels that include missing:
 all_missing_labels <-
-  dictionary[grep("Missing", value_label), "value_label", with = F]
+  rbind(
+    dictionary19[grep("Missing", value_label), "value_label", with = F],
+    dictionary21[grep("Missing", value_label), "value_label", with = F],
+    use.names = T)
+
 
 # both as character:
 all_missing_vector <- unique(rbind(all_missing_codes, all_missing_labels, use.names = F))
@@ -129,26 +161,47 @@ replace_survey_missing <- function(dat) {
   return(na_dat)
 }
 
-day <- replace_survey_missing(day)
-trip <- replace_survey_missing(trip)
-hh <- replace_survey_missing(hh)
-per <- replace_survey_missing(per)
-veh <- replace_survey_missing(veh)
+day19 <- replace_survey_missing(day19)
+trip19 <- replace_survey_missing(trip19)
+hh19 <- replace_survey_missing(hh19)
+per19 <- replace_survey_missing(per19)
+veh19 <- replace_survey_missing(veh19)
+
+day21 <- replace_survey_missing(day21)
+trip21 <- replace_survey_missing(trip21)
+hh21 <- replace_survey_missing(hh21)
+per21 <- replace_survey_missing(per21)
+veh21 <- replace_survey_missing(veh21)
 
 rm(all_missing_labels, all_missing_vector, all_missing_codes)
 
 # Set IDs as Integer64 -----------
 message("IDs as Int64")
-hh[, hh_id := as.integer64(hh_id)]
-veh[, hh_id := as.integer64(hh_id)]
-day[, c("hh_id", "person_id") := lapply(.SD, as.integer64),
+hh19[, hh_id := as.integer64(hh_id)]
+hh21[, hh_id := as.integer64(hh_id)]
+
+veh19[, hh_id := as.integer64(hh_id)]
+veh21[, hh_id := as.integer64(hh_id)]
+
+day19[, c("hh_id", "person_id") := lapply(.SD, as.integer64),
   .SDcols = c("hh_id", "person_id")
 ]
-trip[, c("hh_id", "person_id", "trip_id") := lapply(.SD, as.integer64),
+day21[, c("hh_id", "person_id") := lapply(.SD, as.integer64),
+    .SDcols = c("hh_id", "person_id")
+]
+
+trip19[, c("hh_id", "person_id", "trip_id") := lapply(.SD, as.integer64),
   .SDcols = c("hh_id", "person_id", "trip_id")
 ]
-per[, c("hh_id", "person_id") := lapply(.SD, as.integer64),
+trip21[, c("hh_id", "person_id", "trip_id") := lapply(.SD, as.integer64),
+     .SDcols = c("hh_id", "person_id", "trip_id")
+]
+
+per19[, c("hh_id", "person_id") := lapply(.SD, as.integer64),
   .SDcols = c("hh_id", "person_id")
+]
+per21[, c("hh_id", "person_id") := lapply(.SD, as.integer64),
+      .SDcols = c("hh_id", "person_id")
 ]
 
 ## Clean up---------------
