8336985ff88c10adaa492e59339c82f791bd26b9
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-06-23 20:30:43 -0500

    reorder levels of mode_group; prevent disordering of mode_type

--- data-raw/09-derive-var-trip-mode-group.R
+++ data-raw/09-derive-var-trip-mode-group.R
@@ -1,64 +1,51 @@
 trip19 <-
   trip19 %>%
-  mutate(mode_type = as.character(mode_type)) %>%
-  mutate(mode_type = ifelse(grepl("bicy", mode_type_detailed), "Bicycle", mode_type)) %>%
+  mutate(mode_type_chr = as.character(mode_type)) %>%
+  mutate(mode_type_chr = ifelse(grepl("bicy", mode_type_detailed), "Bicycle", mode_type_chr)) %>%
   mutate(
     mode_group =
-      recode_factor(mode_type,
+      recode_factor(mode_type_chr,
         `Household vehicle` = "Drive",
-        `School bus` = "Other",
         `Other vehicle` = "Drive",
+        `For-hire vehicle` = "Drive",
+        `Smartphone ridehailing service` = "Drive",
         `Public bus` = "Transit",
-        `Walk` = "Walk",
-        `Bicycle` = "Bicycle",
         `Rail` = "Transit",
+        `Bicycle` = "Bicycle",
+        `Walk` = "Walk",
         `Other bus` = "Other",
         Other = "Other",
-        `Smartphone ridehailing service` = "Drive",
-        `For-hire vehicle` = "Drive",
         `Micromobility` = "Other",
-        `Long distance passenger mode` = "Other"
+        `Long distance passenger mode` = "Other",
+        `School bus` = "Other"
       )
-  )
-
-# 2021 data missing mode_type_detailed
-mode_type_hierarchy <- read.csv('data-raw/mode_type_hierarchy.csv')
-trip21_detailed <- trip21 %>%
-  select(trip_id, mode_1, mode_2, mode_3) %>%
-  pivot_longer(-trip_id, values_to = "mode_type_detailed") %>%
-  select(-name) %>%
-  filter(!is.na(mode_type_detailed)) %>%
-  left_join(mode_type_hierarchy, by = "mode_type_detailed") %>%
-  group_by(trip_id) %>%
-  summarize(mode_type_value = max(mode_type_value)) %>%
-  left_join(mode_type_hierarchy, by = "mode_type_value")
-
+  ) %>%
+  select(-mode_type_chr)
 
 
 trip21 <-
-  trip21 %>%
-  left_join(trip21_detailed, by = "trip_id")
-  mutate(mode_type = as.character(mode_type)) %>%
-  mutate(mode_type = ifelse(grepl("bicy", mode_type_detailed), "Bicycle", mode_type)) %>%
+  mutate(mode_type_chr = as.character(mode_type)) %>%
+  mutate(mode_type_chr = ifelse(grepl("bicy", mode_type_detailed), "Bicycle", mode_type_chr)) %>%
   mutate(
     mode_group =
-      recode_factor(mode_type,
-                    `Walk` = "Walk",
+      recode_factor(mode_type_chr,
+                    `Vehicle` = "Drive",
+                    `Carshare` = "Drive",
+                    `Taxi` = "Drive",
+                    `Smartphone-app ride-hailing service` = "Drive",
+                    `Transit` = "Transit",
                     `Bicycle or e-bicycle` = "Bicycle",
                     `Bike-share` = "Bicycle",
                     `Scooter-share` = "Other",
-                    `Taxi` = "Drive",
-                    `Smartphone-app ride-hailing service` = "Drive",
+                    `Walk` = "Walk",
                     Other = "Other",
-                    `Vehicle` = "Drive",
-                    `Carshare` = "Drive",
                     `School bus` = "Other",
-                    `Shuttle` = "Transit",
                     `Ferry` = "Other",
-                    `Transit` = "Transit",
+                    `Shuttle` = "Other",
                     `Long distance passenger mode` = "Other"
       )
-  )
+  ) %>%
+  select(-mode_type_chr)
 
 
 message("New variable in trip table: mode_group, that condenses mode_type_detailed (but keeps bicycles separate)")
