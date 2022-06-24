49a6f125792534032701f817d8c8f09cfa8d2a84
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-06-23 20:22:34 -0500

    format, rearrange, headers

--- data-raw/02-derive-var-vehicle-id.R
+++ data-raw/02-derive-var-vehicle-id.R
@@ -1,9 +1,6 @@
-### Add vehicle ID -------
+# 2019 -------
 veh19 <-
   veh19 %>% mutate(veh_id = paste(hh_id, "_", vehicle_num, sep = ""))
-
-veh21 <-
-  veh21 %>% mutate(veh_id = paste(hh_id, "_", str_extract(vehicle_num, "[0-9]+"), sep = ""))
 
 trip19 <- trip19 %>%
   mutate(veh_id = case_when(
@@ -27,6 +24,10 @@ trip19 <- trip19 %>%
     "Other Vehicle",
     TRUE ~ veh_id))
 
+# 2021 ----
+## Mode type detailed for 2021 dataset ----
+veh21 <-
+  veh21 %>% mutate(veh_id = paste(hh_id, "_", str_extract(vehicle_num, "[0-9]+"), sep = ""))
 
 mode_type_hierarchy <- read.csv('data-raw/mode_type_hierarchy.csv')
 
@@ -37,16 +38,15 @@ mode_type_detailed21 <-
   select(-name) %>%
   filter(!is.na(mode_type_detailed)) %>%
   unique() %>%
-  left_join(mode_type_hierarchy) %>%
+  left_join(mode_type_hierarchy, by = "mode_type_detailed") %>%
   group_by(trip_id) %>%
   summarize(mode_type_value = max(mode_type_value)) %>%
   ungroup() %>%
-  left_join(mode_type_hierarchy) %>%
+  left_join(mode_type_hierarchy, by = "mode_type_value") %>%
   select(-mode_type_value)
 
-
 trip21 <- trip21 %>%
-  left_join(mode_type_detailed21) %>%
+  left_join(mode_type_detailed21, by = "trip_id") %>%
   mutate(veh_id = case_when(
     grepl(pattern = "Household vehicle", x = mode_type_detailed) ~
       paste(hh_id, str_extract(mode_type_detailed, "[0-9]+"), sep = "_")
