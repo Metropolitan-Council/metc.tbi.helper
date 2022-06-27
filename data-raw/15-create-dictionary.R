cfd4c87f5e456f5c1f821eb203460f695091f41e
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-06-27 11:06:46 -0500

    update for 2021

--- data-raw/15-create-dictionary.R
+++ data-raw/15-create-dictionary.R
@@ -22,52 +22,53 @@
 #   unique() %>%
 #   as.data.table()
 #
-# # Appending missing variables to tbi_dict
-# missing_vars <-
-#   lapply(tbi_tables, function(x) setdiff(names(x), db_dictionary$variable) %>% as.data.frame()) %>%
-#   bind_rows(.id = "which_table") %>%
-#   rename(variable = ".") %>%
-#   mutate(wt_field = case_when(
-#     which_table == "per" ~ "person_weight",
-#     which_table == "hh" ~ "hh_weight",
-#     which_table == "trip" ~ "trip_weight",
-#     which_table == "day" ~ "day_weight",
-#     which_table == "veh" ~ "hh_weight",
-#     which_table == "trip_purpose" ~ "trip_purpose_weight"
-#   ))
-#
-# full_dictionary <- db_dictionary %>%
-#   # we'll probably want to play with the ORDERING of this case-when command
-#   # to assign variables to tables when they appear in multiple tables.
-#   mutate(which_table = case_when(
-#     variable %in% names(per) ~ "per",
-#     variable %in% names(hh) ~ "hh",
-#     variable %in% names(trip) ~ "trip",
-#     variable %in% names(day) ~ "day",
-#     variable %in% names(veh) ~ "veh",
-#     variable %in% names(trip_purpose) ~ "trip_purpose"
-#   )) %>%
-#   # find the weighting field for each table:
-#   mutate(wt_field = case_when(
-#     which_table == "per" ~ "person_weight",
-#     which_table == "hh" ~ "hh_weight",
-#     which_table == "trip" ~ "trip_weight",
-#     which_table == "day" ~ "day_weight",
-#     which_table == "veh" ~ "hh_weight",
-#     which_table == "trip_purpose" ~ "trip_purpose_weight"
-#   )) %>%
-#   # join to our missing variables:
-#   full_join(missing_vars) %>%
-#   # remove survey metadata fields:
-#   filter(!category %in% c("Survey metadata"))
-#
-# write.csv(full_dictionary, "data-raw/full_dictionary_to_fill.csv")
+# Appending missing variables to tbi_dict
+missing_vars <-
+  lapply(tbi21[c("per", "trip", "hh", "day", "veh")], function(x) setdiff(names(x), tbi21$dictionary$variable) %>% as.data.frame()) %>%
+  bind_rows(.id = "which_table") %>%
+  rename(variable = ".") %>%
+  mutate(wt_field = case_when(
+    which_table == "per" ~ "person_weight",
+    which_table == "hh" ~ "hh_weight",
+    which_table == "trip" ~ "trip_weight",
+    which_table == "day" ~ "day_weight",
+    which_table == "veh" ~ "hh_weight",
+    which_table == "trip_purpose" ~ "trip_purpose_weight"
+  ))
+
+full_dictionary <- tbi21$dictionary %>%
+  # we'll probably want to play with the ORDERING of this case-when command
+  # to assign variables to tables when they appear in multiple tables.
+  mutate(which_table = case_when(
+    variable %in% names(tbi21$per) ~ "per",
+    variable %in% names(tbi21$hh) ~ "hh",
+    variable %in% names(tbi21$trip) ~ "trip",
+    variable %in% names(tbi21$day) ~ "day",
+    variable %in% names(tbi21$veh) ~ "veh",
+    variable %in% names(tbi21$trip_purpose) ~ "trip_purpose"
+  )) %>%
+  # find the weighting field for each table:
+  mutate(wt_field = case_when(
+    which_table == "per" ~ "person_weight",
+    which_table == "hh" ~ "hh_weight",
+    which_table == "trip" ~ "trip_weight",
+    which_table == "day" ~ "day_weight",
+    which_table == "veh" ~ "hh_weight",
+    which_table == "trip_purpose" ~ "trip_purpose_weight"
+  )) %>%
+  # join to our missing variables:
+  full_join(missing_vars) %>%
+  mutate(category = "")
+  # remove survey metadata fields:
+  # filter(!category %in% c("Survey metadata"))tbi21$
+
+write.csv(full_dictionary, "data-raw/full_dictionary_to_fill_2021.csv")
 
 
 # some work by hand occurred:
-dictionary <- read.csv("data-raw/full_dictionary_filled.csv")
+dictionary21 <- read.csv("data-raw/full_dictionary_filled.csv")
 
-dictionary <- dictionary %>%
+dictionary21 <- dictionary21 %>%
   filter(!category %in% c("Survey metadata", "PII"))
 
 ## Clean up---------------
