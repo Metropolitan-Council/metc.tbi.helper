8c6200adb751c9da1caae6862f70a6eea7b5036d
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-06-28 11:28:48 -0500

    reverse order of load/write

--- data-raw/99-survey-data-to-csv.R
+++ data-raw/99-survey-data-to-csv.R
@@ -11,11 +11,12 @@ for(i in names(tbi19)){
   write.csv(tbi19[[i]], paste0(directory_to_save, i, "-2019", ".csv"), row.names = F)
 }
 
+# load 2021 data ----
+load(url(paste0("https://github.com/Metropolitan-Council/metc.tbi.helper/raw/main/data/",
+                "tbi21.rda")))
+
 # Write 2021 data ----
 for(i in names(tbi21)){
   write.csv(tbi19[[i]], paste0(directory_to_save, i, "-2021", ".csv"), row.names = F)
 }
 
-# load 2021 data ----
-load(url(paste0("https://github.com/Metropolitan-Council/metc.tbi.helper/raw/main/data/",
-                "tbi21.rda")))
