4e8a54397a78ec7e0c2c7247072220eeac789d0f
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-06-28 11:25:44 -0500

    rename; add URLs; clean up

---
+++ data-raw/99-survey-data-to-csv.R
@@ -0,0 +1,21 @@
+library(tidyr)
+library(devtools)
+directory_to_save <- "data/"
+
+# load 2019 data ----
+load(url(paste0("https://github.com/Metropolitan-Council/metc.tbi.helper/raw/main/data/",
+                "tbi19.rda")))
+
+# Write 2019 data ----
+for(i in names(tbi19)){
+  write.csv(tbi19[[i]], paste0(directory_to_save, i, "-2019", ".csv"), row.names = F)
+}
+
+# Write 2021 data ----
+for(i in names(tbi21)){
+  write.csv(tbi19[[i]], paste0(directory_to_save, i, "-2021", ".csv"), row.names = F)
+}
+
+# load 2021 data ----
+load(url(paste0("https://github.com/Metropolitan-Council/metc.tbi.helper/raw/main/data/",
+                "tbi21.rda")))
