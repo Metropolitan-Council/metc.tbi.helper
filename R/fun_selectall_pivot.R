322ebc3e74bcca9a831329205d4232db455f45fd
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-07-05 10:30:53 -0500

    rename function; include _num as well as _id columns

--- R/fun_selectall_pivot.R
+++ R/fun_selectall_pivot.R
@@ -1,11 +1,25 @@
 selectall_pivot <-
 function(surveyobj, table, variable_prefix) {
+
+  labels <- surveyobj %>%
+    pluck("dictionary") %>%
+    filter(grepl(pattern = variable_prefix, x = variable)) %>%
+    select(variable, variable_label) %>%
+    unique()
+
+
   surveyobj %>%
     pluck(table) %>%
-    select(contains("_id"), contains("weight"), starts_with(variable_prefix)) %>%
+    select(contains("_id"), contains("_num"), contains("weight"),
+           starts_with(variable_prefix)) %>%
     pivot_longer(
-      cols = starts_with(variable_prefix), names_prefix = paste0(variable_prefix, "_")
-    )
+      cols = starts_with(variable_prefix),
+      # names_prefix = paste0(variable_prefix, "_"),
+      names_to = "variable"
+    ) %>%
+    left_join(labels)
+
 }
 
-pivotlong_selectall(tbi19, "day", "no_travel")
+# selectall_pivot(tbi19, "day", "no_travel")
+
