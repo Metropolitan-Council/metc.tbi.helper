55b7bd1e3571a0667ada45c7c711fedc4db883a7
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-07-05 10:31:38 -0500

    get dictionary names to match up across years`

--- data-raw/_data-compile.R
+++ data-raw/_data-compile.R
@@ -46,6 +46,8 @@ source("data-raw/14-remove-pii.R")
 dictionary19 <- read.csv("data-raw/final_dictionary_2019.csv")
 # dictionary21 <- read.csv("final_dictionary_2021.csv")
 
+dictionary21 <- dictionary21 %>% rename(variable_label = description)
+
 # Put data in lists -------------------------
 tbi19 <- list(
   "day" = day19,
