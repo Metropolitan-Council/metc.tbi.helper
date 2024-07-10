5e768313a005f9c7a559aba14ce238dcd37473de
cynthiazhangmt <135662386+cynthiazhangmt@users.noreply.github.com>
2024-07-10 14:27:38 -0500

    Delete qa_qc folder

--- qa_qc/00_load_pkgs.R
+++
@@ -1,4 +0,0 @@
-suppressMessages(library(data.table, quietly = T))
-suppressMessages(library(leaflet, quietly = T))
-suppressMessages(library(plotly, quietly = T))
-suppressMessages(library(councilR, quietly = T))
