3a01215a3069a160ddc66477b0c20f23cfc7fc76
cynthiazhangmt <xinran.zhang@metc.state.mn.us>
2024-07-10 13:35:10 -0500

    update data loading path

--- data-raw/01-get-survey-data.R
+++ data-raw/01-get-survey-data.R
@@ -1,9 +1,10 @@
 # Load data from local file
 # MetC staff only
-system("open smb://rafsshare.mc.local/shared/MTS/")
-qs::qload("/Volumes/MTS/Working/Modeling/Household_Survey_Data/Dataset_2024-05-13/CombinedDataset_2024-05-13/dat_all_upcoded_labeled.qs")
+# see Brandon/Liz to update the R environment file
+qs::qload(file.path(Sys.getenv("path_to_tbi"), "dat_all_upcoded_labeled.qs"))
 tbi <- dat_all_upcoded_labeled
 rm(dat_all_upcoded_labeled)
 
-val_list <- fread("/Volumes/MTS/Working/Modeling/Household_Survey_Data/Dataset_2024-05-13/WeightedData_2024-05-13/value_labels.csv")
-var_list <- fread("/Volumes/MTS/Working/Modeling/Household_Survey_Data/Dataset_2024-05-13/WeightedData_2024-05-13/variable_list.csv")
+val_list <- fread(file.path(Sys.getenv("path_to_tbi_wt"), "value_labels.csv"))
+var_list <- fread(file.path(Sys.getenv("path_to_tbi_wt"), "variable_list.csv"))
+
