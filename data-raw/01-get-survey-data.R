552f7eb24d7557334af521b954847d33f54eb4f1
cynthiazhangmt <xinran.zhang@metc.state.mn.us>
2024-07-17 13:49:51 -0500

    clean data

--- data-raw/01-get-survey-data.R
+++ data-raw/01-get-survey-data.R
@@ -8,3 +8,5 @@ rm(dat_all_upcoded_labeled)
 val_list <- fread(file.path(Sys.getenv("path_to_tbi_wt"), "value_labels.csv"))
 var_list <- fread(file.path(Sys.getenv("path_to_tbi_wt"), "variable_list.csv"))
 
+# cleaning table
+tbi$trip[pt_density == "Inf", pt_density := 0][point_dist_index == 0, point_dist_index := NA]
