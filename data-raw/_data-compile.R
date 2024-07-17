14e23a1602ac6e14426a991e9bd9da9eb69aafe5
cynthiazhangmt <xinran.zhang@metc.state.mn.us>
2024-07-17 14:12:27 -0500

    relabel the scripts

--- data-raw/_data-compile.R
+++ data-raw/_data-compile.R
@@ -20,15 +20,15 @@ source("data-raw/03-get-geographic-boundaries.R")
 # source("data-raw/05-get-dps-vehicle-weight-data.R")
 
 # Extra variables ------
-source("data-raw/06-derive-var-person-race.R")
-source("data-raw/07-derive-var-hh-income-easyread.R")
+source("data-raw/04-derive-var-person-race.R")
+source("data-raw/05-derive-var-hh-income-easyread.R")
 
 
 # source("data-raw/08-derive-var-trip-mode-group.R") # deprecated
 # source("data-raw/09-derive-var-trip-distance-adjusted.R") # Analysis needed
 # source("data-raw/10-derive-table-trip-purpose.R")
-source("data-raw/11-derive-var-trip-purpose-broad.R")
-source("data-raw/12-derive-var-trip-seasons.R")
-source("data-raw/13-remove-pii.R")
+source("data-raw/06-derive-var-trip-purpose-broad.R")
+source("data-raw/07-derive-var-trip-seasons.R")
+source("data-raw/08-remove-pii.R")
 # source("data-raw/14-make-data-objects.R")
 source("data-raw/99-distribute-data.R")
