183d14b0bc381abdce181d24bb809169d16b98e7
Brandon Whited <brandon.whited@metc.state.mn.us>
2024-08-07 16:00:10 -0500

    nothing update

--- data-raw/01-get-survey-data.R
+++ data-raw/01-get-survey-data.R
@@ -1,6 +1,7 @@
 # Load data from local file
 # MetC staff only
 # see Brandon/Liz to update the R environment file
+source("data-raw/00-load-pkgs.R")
 qs::qload(file.path(Sys.getenv("path_to_tbi"), "dat_all_upcoded_labeled.qs"))
 tbi <- dat_all_upcoded_labeled
 rm(dat_all_upcoded_labeled)
@@ -10,3 +11,5 @@ var_list <- fread(file.path(Sys.getenv("path_to_tbi_wt"), "variable_list.csv"))
 
 # cleaning table
 tbi$trip[pt_density == "Inf", pt_density := 0][point_dist_index == 0, point_dist_index := NA]
+
+
