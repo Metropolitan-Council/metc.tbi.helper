6e16c9f44dc1e0d6ad366ca1bb8a4e80e6d17476
Kenton Briggs <kenton.briggs@metc.state.mn.us>
2024-10-17 12:45:59 -0500

    creating r script for trip purpose

---
+++ presentation/trip_purpose.r
@@ -0,0 +1,12 @@
+source("presentation/_load_libraries.R")
+if(!exists("tbi")) source("presentation/_load_data.R")
+
+# trip purpose x year --------------
+
+# trip purpose x income x 2023 --------------
+
+# trip purpose x race x 2023 --------------
+
+# trip purpose x age x 2023 --------------
+
+# trip purpose x hhsize x 2023 --------------
