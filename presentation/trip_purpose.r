4ba1f007fcd3033d984a45c18c2ef78bf702a290
Kenton Briggs <kenton.briggs@metc.state.mn.us>
2024-10-18 15:25:49 -0500

    end of day update

--- presentation/trip_purpose.r
+++ presentation/trip_purpose.r
@@ -4,65 +4,55 @@ if(!exists("tbi")) source("presentation/_load_data.R")
 
 #trip purpose setup
 mapping <- c(
-  "Not imputable" = "Errand/Misc/Shopping",
-  "Home" = "Errand/Misc/Shopping",
+  "Not imputable" = "Errand/Shopping/Misc",
+  "Home" = "Errand/Shopping/Misc",
   "Work" = "Work",
   "Work related" = "Work",
   "School" = "Education",
   "School related" = "Education",
   "Escort" = "Escort",
-  "Shopping" = "Errand/Misc/Shopping",
+  "Shopping" = "Errand/Shopping/Misc",
   "Meal" = "Dining",
   "Social/Recreation" = "Social/Rec",
-  "Errand" = "Errand/Misc/Shopping",
-  "Overnight" = "Errand/Misc/Shopping",
-  "Missing" = "Missing",
-  "Other" = "Errand/Misc/Shopping",
-  "NA" = "Missing"
+  "Errand" = "Errand/Shopping/Misc",
+  "Overnight" = "Errand/Shopping/Misc",
+  "Missing" = "Unknown",
+  "Other" = "Errand/Shopping/Misc",
+  "NA" = "Unknown"
 )
-trip_purpose <- merge(tbi$trip_purpose, tbi$hh, by = c("hh_id", "survey_year"))
-trip_purpose[, purpose_category := mapping[purpose_category]]
 
 
 # trip purpose x year --------------
-trip_purpose <-
-  trip_purpose %>%
+purpose_year <-
+  tbi$trip_purpose %>%
+  mutate(purpose_mapped = mapping[purpose_category]) %>%
+  left_join(tbi$hh) %>%
   as_survey_design(
     weights = "trip_purpose_weight",
     ids = "linked_trip_id",
     strata = "sample_segment"
   ) %>%
-  group_by(purpose_category) %>%
+  group_by(survey_year, purpose_mapped) %>%
   summarize(prop = survey_prop(proportion = TRUE, se = TRUE))
 
+#purpose_year[,sum(prop),survey_year] Check to make sure prop equals 1, setDT first
 
-plot_ly() %>%
-  add_bars(data = trip_purpose["purpose_category" != "Missing"]
-           , y = ~prop
-           , x = ~ purpose_category
-           , color = ~survey_year
-           , colors = c(colors$councilBlue)
-           , textfont = list(size = 11, color = "white")
-           , textangle = 0
-           , textposition = "inside"
-  ) %>%
+year_plot <- plot_ly(
+  data = purpose_year,
+  x = ~purpose_mapped,
+  y = ~prop,
+  color = ~as.factor(survey_year),
+  type = "bar",
+  colors = c(colors$councilBlue, colors$esBlue)
+) %>%
   layout(
-    title = "Trip Purpose"
-    # , barmode = "relative"
-    , font = list(size = 16)
-    , yaxis = list(
-      # showticklabels = F,
-      # showgrid = F,
-      title = "Weekday Trips"
-    )
-    , xaxis = list(
-      title = NA
-    )
-    , margin = list(t = 45)
-    , uniformtext = list(minsize = 12, mode = F)
-    # , legend = list(traceorder = "reversed")
+    xaxis = list(title = "Trip Purpose"),
+    yaxis = list(title = "Proportion of Trips", tickformat = ".0%"),
+    barmode = "group",
+    title = "Trip Purpose Proportions by Year"
   ) %>%
   print
+
 # trip purpose x income x 2023 --------------
 
 # trip purpose x race x 2023 --------------
